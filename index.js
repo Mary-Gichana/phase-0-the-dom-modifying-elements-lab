8fd63fd1fce2ce29eb45f67515f695854e2c8e15
Mary Monchari Gichana <marygichana95@gmail.com>
2024-10-06 18:08:05 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+
+const h1 = document.createElement("h1");
+const newHeader = h1;
+
+newHeader.id = "victory";
+newHeader.textContent = "Mary Gichana is the champion";
